3ce87cc7f9945da3e5adf1cf0a3a063fb6d72fe3
Uriel <urielferraz04@gmail.com>
2024-04-29 19:18:11 -0300

    q2 js

--- 2/script.js
+++ 2/script.js
@@ -1,3 +1,10 @@
+let base = prompt("Digite a base do retângulo:");
+let altura = prompt("Digite a altura do retângulo:");
 
+base = parseFloat(base);
+altura = parseFloat(altura);
 
+const area = base * altura;
 
+console.log ("A área do retângulo é:", area);
+alert ('área: ' + area); 
