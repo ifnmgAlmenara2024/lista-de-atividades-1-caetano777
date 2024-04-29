7efe4e0eae0736be5229e5d5069c86b3fb709ab1
Uriel <urielferraz04@gmail.com>
2024-04-29 19:24:02 -0300

    q3 js

--- 3/script.js
+++ 3/script.js
@@ -1,3 +1,8 @@
+let temperatura = prompt("Digite a temperatura em graus celsius: ");
 
+temperatura = parseFloat(temperatura);
 
+const conversao = ((temperatura * 9/5) + 32)
 
+console.log ("A temperetura convertida para Celsius em Fahrenheit: ", conversao);
+alert ('Graus celsius: ' +temperatura +'/ Celsius em Fahrenheit: ' +conversao); 
